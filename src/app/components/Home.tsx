4fe02c4100b306961aa22232172d2a2d96eca8b8
HadiqaGohar <160311280+HadiqaGohar@users.noreply.github.com>
2025-09-10 22:14:07 +0500

    Update Home.tsx

--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -25,11 +25,11 @@ const HomePage = () => {
           
           {/* Chat With AI */}
           <Link
-            href="/chatbot"
+            href="/services/symptom-analyzer"
             className="w-full sm:w-auto inline-flex items-center justify-center px-8 sm:px-10 py-3 text-base sm:text-lg font-semibold text-white bg-blue-500 rounded-full shadow-lg hover:bg-blue-700 transition-colors duration-200"
           >
             <FaUserDoctor className="mr-2 text-lg sm:text-xl" />
-            Chat with AI
+            Symptom Analyzer
           </Link>
           
           {/* Contact Now */}
